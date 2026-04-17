1d52c1570ddb27f59e930a3dfa27bb3cc73153e0
jafcthedevp <flores.666.anthony@gmail.com>
2026-04-17 10:40:24 -0500

    chore: cleanup diagnostic and test files

--- diag_env.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import os, sys
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "backend"))
-from odoo_connector import _load_dotenv_once
-res = _load_dotenv_once()
-for r in res:
-    print(r)
-print("ODOO_WEB_PASSWORD =", os.environ.get("ODOO_WEB_PASSWORD"))
